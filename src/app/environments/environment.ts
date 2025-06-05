405acdf30753f71c5f7d04f7cc46a427beadb786
DfmcT <dfmc363@gmail.com>
2025-06-05 11:07:32 +0200

    Probando la pagina

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'http://localhost:5000'
-  apiBaseUrl: '/api'
+  apiBaseUrl: 'http://localhost:5000' // PARA FUNCIONAMIENTO VPS
+  // apiBaseUrl: '/api' // PARA FUNCIONAMIENTO LOCAL 
+
 };
