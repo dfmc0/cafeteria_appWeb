8bb5d6981b18c5903b6c24166205d63062aaed1b
DfmcT <dfmc363@gmail.com>
2025-06-05 16:50:17 +0200

    Conexión con API

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://vps793825.ovh.net:5000' // PARA FUNCIONAMIENTO VPS
+  apiBaseUrl: 'http://localhost:5000/' // PARA FUNCIONAMIENTO LOCAL
+  // apiBaseUrl: 'http://vps793825.ovh.net:5000' // PARA FUNCIONAMIENTO VPS
   // apiBaseUrl: '/api' // PARA FUNCIONAMIENTO LOCAL 
 
 };
